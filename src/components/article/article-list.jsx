063b25ad53312f0aa4c09e244fc194bfe7b89f7c
Shane Upchurch <shaneupchurch@gmail.com>
2021-02-03 15:37:33 -0600

    article list presentational created

---
+++ src/components/article/article-list.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Article from './single-article';
+
+const ArticleList = ({ articles }) => {
+  const articleInfo = articles.map(article => (
+    <li key={article.publishedAt}>
+      <Article {...article} />
+    </li>
+  ));
+
+  return (
+    <ul>
+      {articleInfo}
+    </ul>
+  );
+};
+
+ArticleList.propTypes = {
+  articles: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    publishedAt: PropTypes.string.isRequired
+  })).isRequired
+};
+
+export default ArticleList;
