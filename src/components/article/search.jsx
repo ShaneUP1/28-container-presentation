1d4054755249e0240f091bc6a0dbf9f4276dca1f
Shane Upchurch <shaneupchurch@gmail.com>
2021-02-03 15:19:35 -0600

    corrects filenames

--- src/components/article/search.jsx
+++ src/components/article/search.jsx
@@ -1,14 +1,1 @@
-import React from 'react';
-import PropTypes from 'prop-types';
 
-const Article = ({ title }) => (
-  <div>
-    <h2>{title}</h2>
-  </div>
-);
-
-Article.propTypes = {
-  title: PropTypes.string.isRequired
-};
-
-export default Article;
