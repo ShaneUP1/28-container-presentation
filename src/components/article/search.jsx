5b032b045a156bd2da62408d532f4c565e04d75b
Shane Upchurch <shaneupchurch@gmail.com>
2021-02-03 15:50:07 -0600

    searchbox presentational built

--- src/components/article/search.jsx
+++ src/components/article/search.jsx
@@ -1,1 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const searchBox = ({ onChange }) => {
+  <input
+    type="text"
+    onChange={onChange}
+  />;
+};
+
+searchBox.propTypes = {
+  onChange: PropTypes.func.isRequired
+};
+
 
